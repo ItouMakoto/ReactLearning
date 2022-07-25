ac4e8ad2b3fd9e6f1dab16490f2063803cacc526
itoumakoto <itoumakoto@192.168.0.100>
2022-07-25 21:38:07 +0800

    feat: 🎸 添加状态控制示例

--- react-learning/src/02state/UseState.js
+++ react-learning/src/02state/UseState.js
@@ -1,11 +1,32 @@
 import React, {Component} from 'react';
 
 class UseState extends Component {
+    // state={
+    //     iscollection:false,
+    // }
+    constructor() {
+        super();
+        this.state={
+            iscollection:false,
+        }
+    }
+    showWords=()=>{
+        return (
+            this.state.iscollection===false && <div>已经收藏</div>
+        )
+    }
     render() {
         return (
             <div>
                 <h1>欢迎来到夜之城</h1>
-                <button>收藏</button>
+                <button onClick={
+                    ()=>{
+                        this.setState({iscollection:!this.state.iscollection})
+                    }
+
+                }>{this.state.iscollection?"取消收藏":"收藏"}</button>
+                {/*{this.state.iscollection===false && <div>已经收藏</div>}*/}
+                {this.showWords()}
             </div>
         );
     }
--- react-learning/src/index.js
+++ react-learning/src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import {createRoot} from "react-dom/client";
 import OnEvent from "./01event/onEvent";
 import ComponentStyle from "./componentStyle"
+import UseState from "./02state/UseState";
 
 
 
 const root = createRoot(document.getElementById('root'))
-root.render(<OnEvent></OnEvent>)
+root.render(<UseState></UseState>)
